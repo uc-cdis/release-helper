17ad225de2a917a466f018608344bd142848fa62
Fantix King <fantix@uchicago.edu>
2019-03-07 15:21:07 -0600

    feat(github): update GitHub Release
    * Also made a PyPI package

--- setup.py
+++ setup.py
@@ -1,7 +1,26 @@
+from subprocess import check_output
+
 from setuptools import setup
+
+
+def get_version():
+    # https://github.com/uc-cdis/dictionaryutils/pull/37#discussion_r257898408
+    try:
+        tag = check_output(
+            ["git", "describe", "--tags", "--abbrev=0", "--match=[0-9]*"]
+        )
+        return tag.decode("utf-8").strip("\n")
+    except Exception:
+        raise RuntimeError(
+            "The version number cannot be extracted from git tag in this source "
+            "distribution; please either download the source from PyPI, or check out "
+            "from GitHub and make sure that the git CLI is available."
+        )
+
 
 setup(
     name="gen3git",
+    version=get_version(),
     description="Helps with release.",
     license="Apache",
     py_modules=["gen3git"],
